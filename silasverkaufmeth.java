75c9e15765e68166575d78e1439b09120f32fcfa
XiJinpingforreal <93386195+XiJinpingforreal@users.noreply.github.com>
2021-11-10 13:40:40 +0100

    Create silasverkaufmeth.java

---
+++ silasverkaufmeth.java
@@ -0,0 +1,23 @@
+public class Mensch
+	{
+		//Attribute
+	private	int personalnr = 12324;
+	private	boolean isstbanane = false;
+	private String name = "Otto Normie";
+	
+	//constructor
+	public Mensch(int personalnr_neu,boolean isstbanane_neu, String name_neu)
+	{
+		personalnr = personalnr_neu;
+		isstbanane = isstbanane_neu;
+		name = name_neu;
+	}
+	//Methoden
+	public void issteinebanane()
+	{
+		isstbanane = true;
+	}
+	public void ändereperso(int status)
+	{
+	personalnr = status;
+	}
